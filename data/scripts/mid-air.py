3799812e1e395116a135a91b5b5dffb630c69d98
sam ly <nghialy@cpp.edu>
2025-06-24 16:23:17 -0700

    hdf5 -> df

--- data/scripts/mid-air.py
+++ data/scripts/mid-air.py
@@ -1,27 +1,25 @@
 import h5py
 from h5py import Group, Dataset
+import pandas as pd
 
-f = h5py.File("data/mid-air/MidAir/Kite_training/cloudy/sensor_records.hdf5", "r")
-
-keys = list(f.keys())
-
-# Jank but we know these are groups
-grp: Group = f[keys[0]] # type: ignore
-
-print(grp.keys())
-
-ground: Group = grp["groundtruth"] # type: ignore
-print(ground.keys())
-
-pos: Dataset = ground["position"] # type: ignore
-
-print(pos.shape)
-print("pos at zero: ", pos[0])
+# MID-AIR position data is sampled at 100hz
+path = "data/mid-air/MidAir/Kite_training/cloudy/sensor_records.hdf5"
+f = h5py.File(path, "r")
 
 gts: list[str] = []
 def get_positions(name: str):
-    if "groundtruth/position" in name:
+    if ("groundtruth/position" in name):
         gts.append(name)
         
 f.visit(get_positions)
-print("visit gts", gts, type(gts))
+
+for gt in gts:
+    print(gt)
+
+item  = f[gts[0]]
+if not isinstance(item, Dataset):
+    raise TypeError("Not a dataset")
+
+print("item", item)
+
+df: pd.DataFrame = pd.DataFrame(item)
